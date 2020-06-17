e857253a1b0d3d5abaa30a9b8f951861f1c80afa
tanimutomo <tanimutomo@gmail.com>
2020-06-17 16:38:52 +0900

    Add db connection check to main.go

--- cmd/api/server/main.go
+++ cmd/api/server/main.go
@@ -2,9 +2,20 @@ package main
 
 import (
 	"net/http"
+
+	"github.com/ispec-inc/go-distributed-monolith/pkg/config"
+	"github.com/ispec-inc/go-distributed-monolith/pkg/infra/dao"
 )
 
 func main() {
+	config.LoadEnv("")
+	rds := config.NewRDS()
+	db, err := dao.NewRDSDB(rds)
+	if err != nil {
+		panic(err)
+	}
+	db.Close()
+
 	r := SetRouter()
 	http.ListenAndServe(":9000", r)
 }
