604f6dbf81f4463f99171b9241e8f3a1e642d2e0
tanimutomo <tanimutomo@gmail.com>
2020-06-17 16:38:39 +0900

    Separate NewRDSDB function to dao/db.go

--- pkg/registry/repository.go
+++ pkg/registry/repository.go
@@ -1,9 +1,6 @@
 package registry
 
 import (
-	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/mysql"
-
 	"github.com/ispec-inc/go-distributed-monolith/pkg/config"
 	"github.com/ispec-inc/go-distributed-monolith/pkg/infra/dao"
 	"github.com/ispec-inc/go-distributed-monolith/src/account/invitation"
@@ -11,13 +8,8 @@ import (
 
 func NewInvitationUsecase() (invitation.Usecase, error) {
 	rds := config.NewRDS()
-	db, err := newRDSDB(rds)
+	db, err := dao.NewRDSDB(rds)
 	return invitation.NewUsecase(
 		dao.NewInvitation(db),
 	), err
-}
-
-func newRDSDB(rds config.RDS) (*gorm.DB, error) {
-	db, err := gorm.Open(rds.DBMS, rds.CONNECT)
-	return db, err
 }
