937b1df3f714dc8482c1306f6c60d92fe2d2f8cb
tanimutomo <tanimutomo@gmail.com>
2020-06-17 16:38:08 +0900

    Fix column def bugs

--- pkg/infra/entity/invitation.go
+++ pkg/infra/entity/invitation.go
@@ -8,6 +8,6 @@ type Invitation struct {
 	ID        int64      `gorm:"column:id; type:bigint(20) auto_increment; not null; primary_key"`
 	UserID    int64      `gorm:"column:user_id"`
 	Code      string     `gorm:"column:code"`
-	CreatedAt *time.Time `gorm:"created_at; not null"`
-	UpdatedAt *time.Time `gorm:"updated_at; not null"`
+	CreatedAt *time.Time `gorm:"column:created_at; not null"`
+	UpdatedAt *time.Time `gorm:"column:updated_at; not null"`
 }
