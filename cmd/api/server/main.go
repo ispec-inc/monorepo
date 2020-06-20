89ce209e6b12d3caae31bbf4bc6138e22bc7e325
tanimutomo <tanimutomo@gmail.com>
2020-06-20 17:01:43 +0900

    Remove db connection from main

--- cmd/api/server/main.go
+++ cmd/api/server/main.go
@@ -2,17 +2,9 @@ package main
 
 import (
 	"net/http"
-
-	"github.com/ispec-inc/go-distributed-monolith/pkg/mysql"
 )
 
 func main() {
-	db, err := mysql.NewDB()
-	if err != nil {
-		panic(err)
-	}
-	db.Close()
-
 	r := SetRouter()
 	http.ListenAndServe(":9000", r)
 }
