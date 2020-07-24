03d5a2fbac608869dadc35eb1f9a84ab4633972d
tanimutomo <tanimutomo@gmail.com>
2020-07-24 15:19:57 +0900

    Add testfixture to dao's test

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.13
 
 require (
 	github.com/go-chi/chi v4.1.2+incompatible
+	github.com/go-testfixtures/testfixtures/v3 v3.3.0
 	github.com/golang/mock v1.4.3
 	github.com/jinzhu/gorm v1.9.14
 	github.com/joho/godotenv v1.3.0
--- pkg/infra/dao/invitation_test.go
+++ pkg/infra/dao/invitation_test.go
@@ -1,18 +1,27 @@
 package dao
 
 import (
+	"database/sql"
+	"fmt"
+	"os"
 	"testing"
 
+	"github.com/go-testfixtures/testfixtures/v3"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 	"github.com/stretchr/testify/assert"
 
+	"github.com/ispec-inc/go-distributed-monolith/pkg/apperror"
 	"github.com/ispec-inc/go-distributed-monolith/pkg/config"
 	"github.com/ispec-inc/go-distributed-monolith/pkg/domain/model"
-	"github.com/ispec-inc/go-distributed-monolith/pkg/infra/entity"
 	"github.com/ispec-inc/go-distributed-monolith/pkg/mysql"
-	"github.com/ispec-inc/go-distributed-monolith/pkg/presenter"
 )
 
-func Test(t *testing.T) {
+var (
+	dao   Invitation
+	sqlDB *sql.DB
+)
+
+func TestMain(m *testing.M) {
 	config.Init()
 
 	db, cleanup, err := mysql.Init()
@@ -21,42 +30,78 @@ func Test(t *testing.T) {
 	}
 	defer cleanup()
 
-	db.AutoMigrate(entity.Invitation{})
+	dao = NewInvitation(db)
 
-	// seed data
-	invitation := entity.Invitation{
-		ID:     int64(1),
-		UserID: int64(1),
-		Code:   "invitation-code",
-	}
-	db.Save(&invitation)
-
-	repo := NewInvitation(db)
-	test := NewTestInvitation(t, repo)
-	test.FindSuccess()
-
-	db.Delete(&invitation)
-}
-
-type testInvitation struct {
-	t *testing.T
-	r Invitation
-}
-
-func NewTestInvitation(t *testing.T, r Invitation) testInvitation {
-	t.Helper()
-	return testInvitation{t, r}
-}
-
-func (t testInvitation) FindSuccess() {
-	output, err := t.r.Find(int64(1))
+	sqlDB, err = sql.Open(
+		config.RDS.MS,
+		fmt.Sprintf(
+			"%s:%s@(%s:%s)/%s?charset=utf8mb4&parseTime=true",
+			config.RDS.User, config.RDS.Password,
+			config.RDS.Host, config.RDS.Port,
+			config.RDS.Database,
+		),
+	)
 	if err != nil {
-		t.t.Errorf(
-			"Error: code: %d, message: %s",
-			presenter.CodeStatuses[err.Code()], err.Message(),
-		)
+		panic(err)
 	}
-	assert.IsType(t.t, model.Invitation{}, output)
-	assert.Exactly(t.t, int64(1), output.ID)
-	assert.Exactly(t.t, "invitation-code", output.Code)
+
+	prepareTestData()
+	os.Exit(m.Run())
+}
+
+func prepareTestData() {
+	fixtures, err := testfixtures.New(
+		testfixtures.Database(sqlDB),
+		testfixtures.Dialect(config.RDS.MS),
+		testfixtures.Directory("testdata/invitation"),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	if err := fixtures.Load(); err != nil {
+		panic(err)
+	}
+}
+
+func TestInvitationDao_Find_Success(t *testing.T) {
+	t.Helper()
+
+	output, aerr := dao.Find(int64(1))
+	assert.Exactly(t, nil, aerr)
+	assert.Exactly(t, int64(1), output.ID)
+	assert.Exactly(t, "code", output.Code)
+}
+
+func TestInvitationDao_Fail_NotFound(t *testing.T) {
+	t.Helper()
+
+	_, aerr := dao.Find(int64(2))
+	assert.Exactly(t, apperror.CodeNotFound, aerr.Code())
+}
+
+func TestInvitationDao_Create_Success(t *testing.T) {
+	t.Helper()
+
+	output, aerr := dao.Create(
+		model.Invitation{
+			UserID: int64(2),
+			Code:   "code",
+		},
+	)
+	assert.Exactly(t, nil, aerr)
+	assert.Exactly(t, int64(2), output.UserID)
+	assert.Exactly(t, "code", output.Code)
+}
+
+func TestInvitationDao_Create_Fail_AlreadyExist(t *testing.T) {
+	t.Helper()
+
+	_, aerr := dao.Create(
+		model.Invitation{
+			UserID: int64(1),
+			Code:   "code",
+		},
+	)
+	assert.Exactly(t, apperror.CodeInvalid, aerr.Code())
 }
