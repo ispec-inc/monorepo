e39177f895e9da0016228e994dd71bb2e94faf07
tanimutomo <tanimutomo@gmail.com>
2021-01-11 13:14:06 +0900

    update: handler

--- cmd/api/adapter/invitation/router.go
+++ cmd/api/adapter/invitation/router.go
@@ -4,11 +4,12 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi"
+	"github.com/ispec-inc/go-distributed-monolith/pkg/apphttp"
 )
 
 func NewRouter(handler handler) http.Handler {
 	r := chi.NewRouter()
-	r.Get("/{id}", handler.GetCode)
-	r.Post("/", handler.AddCode)
+	r.Get("/{id}", apphttp.Wrap(handler.GetCode))
+	r.Post("/", apphttp.Wrap(handler.AddCode))
 	return r
 }
