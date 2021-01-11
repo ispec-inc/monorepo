e7b67dea20afaeca0a50842906207292f6868fb0
tanimutomo <tanimutomo@gmail.com>
2021-01-11 13:14:14 +0900

    update: server

--- cmd/api/server/main.go
+++ cmd/api/server/main.go
@@ -3,15 +3,24 @@ package main
 import (
 	"net/http"
 
+	"github.com/ispec-inc/go-distributed-monolith/pkg/apphttp"
 	"github.com/ispec-inc/go-distributed-monolith/pkg/config"
+	"github.com/ispec-inc/go-distributed-monolith/pkg/logger"
 	"github.com/ispec-inc/go-distributed-monolith/pkg/registry"
 )
 
 func main() {
-	config.Init()
-
 	repo, cleanup := registry.NewRepository()
 	defer cleanup()
+
+	var loggerCleanup func()
+	switch config.App.Env {
+	case config.EnvDev:
+		apphttp.Logger = logger.NewLocal()
+	default:
+		apphttp.Logger, loggerCleanup = logger.NewSentry(config.Sentry.DSN, config.Sentry.Env, config.Sentry.Debug)
+	}
+	defer loggerCleanup()
 
 	r := NewRouter(repo)
 	http.ListenAndServe(":9000", r)
