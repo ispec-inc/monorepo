3ddc6ec48afacc640b2d86e1f675fd3c442fd4af
tanimutomo <tanimutomo@gmail.com>
2021-01-16 18:54:58 +0900

    update: loglevel of gorm

--- pkg/mysql/init.go
+++ pkg/mysql/init.go
@@ -11,14 +11,23 @@ import (
 )
 
 func Init() (*gorm.DB, error) {
+	var logLevel logger.LogLevel
+	switch config.App.Env {
+	case config.EnvDev, config.EnvStg:
+		logLevel = logger.Info
+	case config.EnvProd:
+		logLevel = logger.Error
+	}
+
 	conn := fmt.Sprintf(
 		"%s:%s@(%s:%s)/%s?charset=utf8mb4&parseTime=true",
 		config.RDS.User, config.RDS.Password,
 		config.RDS.Host, config.RDS.Port,
 		config.RDS.Database,
 	)
+
 	db, err := gorm.Open(mysql.Open(conn), &gorm.Config{
-		Logger: logger.Default.LogMode(logger.Info),
+		Logger: logger.Default.LogMode(logLevel),
 	})
 	if err != nil {
 		return nil, err
