74e4c22346324510716de925f9a73b3ed224c68d
tanimutomo <tanimutomo@gmail.com>
2021-03-06 20:39:34 +0900

    update: cmds

--- cmd/api/server/main.go
+++ cmd/api/server/main.go
@@ -3,12 +3,7 @@ package main
 import (
 	"net/http"
 
-	"github.com/ispec-inc/go-distributed-monolith/pkg/applog"
-	"github.com/ispec-inc/go-distributed-monolith/pkg/applog/logger"
-	"github.com/ispec-inc/go-distributed-monolith/pkg/config"
 	"github.com/ispec-inc/go-distributed-monolith/pkg/registry"
-	"github.com/ispec-inc/go-distributed-monolith/pkg/sentry"
-	"github.com/ispec-inc/go-distributed-monolith/pkg/stdlog"
 )
 
 func main() {
@@ -18,26 +13,14 @@ func main() {
 	}
 	defer repoCleanup()
 
-	var lgr logger.Logger
-	switch config.App.Env {
-	case config.EnvDev:
-		lgr = stdlog.New()
-	default:
-		l, cleanup, err := sentry.New(
-			sentry.Options{
-				Environment: config.Sentry.DSN,
-				DSN:         config.Sentry.DSN,
-				Debug:       config.Sentry.Debug,
-			},
-		)
-		if err != nil {
-			panic(err)
-		}
-		lgr = l
-		defer cleanup()
+	lgr, lgrCleanup, err := registry.NewLogger()
+	if err != nil {
+		panic(err)
 	}
-	applog.Setup(lgr)
+	defer lgrCleanup()
 
-	r := NewRouter(repo)
+	rgst := registry.NewRegistry(repo, lgr)
+
+	r := NewRouter(rgst)
 	http.ListenAndServe(":9000", r)
 }
