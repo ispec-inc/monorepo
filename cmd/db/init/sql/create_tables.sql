833074bbebf1bca58545ffbd396e5860b0d36146
tanimutomo <tanimutomo@gmail.com>
2021-04-02 21:20:08 +0900

    refactor(mysql): rename Init to New

--- cmd/db/init/sql/create_tables.sql
+++ cmd/db/init/sql/create_tables.sql
@@ -3,5 +3,6 @@ create table if not exists invitations (
   user_id bigint NOT NULL,
   code varchar(255) not null,
   created_at datetime not null,
-  updated_at datetime not null
+  updated_at datetime not null,
+  unique index unique_index_invitations_on_user_id(user_id)
 );
