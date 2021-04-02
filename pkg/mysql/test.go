716636de06c9882b7f0d2eb0adfd92b30bb550ce
tanimutomo <tanimutomo@gmail.com>
2021-04-02 21:21:11 +0900

    feat(mysql): add mysql.NewTest

---
+++ pkg/mysql/test.go
@@ -0,0 +1,31 @@
+package mysql
+
+import (
+	"fmt"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+
+	"github.com/DATA-DOG/go-txdb"
+	"github.com/ispec-inc/go-distributed-monolith/pkg/config"
+)
+
+func NewTest(name string) (*gorm.DB, error) {
+	dsn := fmt.Sprintf(
+		"%s:%s@(%s:%s)/%s?charset=utf8mb4&parseTime=true",
+		config.RDS.User, config.RDS.Password,
+		config.RDS.Host, config.RDS.Port,
+		config.RDS.Database,
+	)
+
+	txdb.Register(name, "mysql", dsn)
+	dialector := mysql.New(mysql.Config{
+		DriverName: name,
+		DSN:        dsn,
+	})
+
+	return gorm.Open(dialector, &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
+}
