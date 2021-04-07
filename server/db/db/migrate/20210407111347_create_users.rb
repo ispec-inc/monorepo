6ecae88b1fa189bd315334c23d3b4e759e85d049
tanimutomo <tanimutomo@gmail.com>
2021-04-07 20:39:08 +0900

    feat: add/server/db

---
+++ server/db/db/migrate/20210407111347_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :name, null: false
+      t.string :description, null: false
+      t.string :email, null: false
+
+      t.timestamps
+
+      t.index :email, unique: true
+    end
+  end
+end
