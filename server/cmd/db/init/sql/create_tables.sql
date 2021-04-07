d2979075740d6c16d114f37d6f3a5a8f0a164499
tanimutomo <tanimutomo@gmail.com>
2021-04-07 20:37:55 +0900

    chore(db): rm cmd/db/init

--- server/cmd/db/init/sql/create_tables.sql
+++
@@ -1,8 +0,0 @@
-create table if not exists invitations (
-  id bigint NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  user_id bigint NOT NULL,
-  code varchar(255) not null,
-  created_at datetime not null,
-  updated_at datetime not null,
-  unique index unique_index_invitations_on_user_id(user_id)
-);
