29b1bbc93df26905831c1e0f8da16178366cb30d
tanimutomo <tanimutomo@gmail.com>
2021-04-08 08:39:27 +0900

    fix: dont ignore server/

---
+++ server/cmd/api/server/middleware.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+
+	"github.com/ispec-inc/go-distributed-monolith/pkg/config"
+)
+
+func commonMiddleware(r *chi.Mux) *chi.Mux {
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.Timeout(config.Router.Timeout))
+	return r
+}
