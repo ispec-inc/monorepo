3295663c93469bc210fe7c13adc0ae04efc1ef08
tanimutomo <tanimutomo@gmail.com>
2021-04-08 16:51:50 +0900

    refactor(server): rm createRequest for unused

--- server/cmd/api/adapter/user/request.go
+++
@@ -1,7 +0,0 @@
-package user
-
-type createRequest struct {
-	Name        string `json:"name" validate:"required"`
-	Description string `json:"description" validate:"required"`
-	Email       string `json:"email" validate:"required"`
-}
