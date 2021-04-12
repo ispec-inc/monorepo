2975bfc691989af89fa2455b51d51ce239d75d13
tanimutomo <tanimutomo@gmail.com>
2021-04-12 15:14:11 +0900

    test(usecase/user): without mockio

--- server/src/user/usecase_test.go
+++ server/src/user/usecase_test.go
@@ -11,16 +11,15 @@ import (
 
 	"github.com/ispec-inc/monorepo/server/pkg/domain/model"
 	mock_repository "github.com/ispec-inc/monorepo/server/pkg/domain/repository/mock"
-	mockio_repository "github.com/ispec-inc/monorepo/server/pkg/domain/repository/mockio"
 )
 
 func TestUserUsecase_Get(t *testing.T) {
 	cases := []struct {
-		name    string
-		give    *GetInput
-		want    *GetOutput
-		userGet mockio_repository.UserGet
-		err     bool
+		name     string
+		give     *GetInput
+		want     *GetOutput
+		userRepo func(*gomock.Controller) *mock_repository.MockUser
+		err      bool
 	}{
 		{
 			name: "success",
@@ -30,20 +29,19 @@ func TestUserUsecase_Get(t *testing.T) {
 			want: &GetOutput{
 				User: &model.User{ID: int64(1)},
 			},
-			userGet: mockio_repository.UserGet{
-				Time:  1,
-				ArgId: int64(1),
-				Ret0:  &model.User{ID: int64(1)},
-				Ret1:  nil,
+			userRepo: func(c *gomock.Controller) *mock_repository.MockUser {
+				u := mock_repository.NewMockUser(c)
+				u.EXPECT().Get(int64(1)).Return(&model.User{ID: int64(1)}, nil).Times(1)
+				return u
 			},
 		},
 		{
 			name: "fail to get user",
 			give: &GetInput{ID: int64(1)},
-			userGet: mockio_repository.UserGet{
-				Time:  1,
-				ArgId: int64(1),
-				Ret1:  errors.New("unknown"),
+			userRepo: func(c *gomock.Controller) *mock_repository.MockUser {
+				u := mock_repository.NewMockUser(c)
+				u.EXPECT().Get(int64(1)).Return(nil, errors.New("unknown")).Times(1)
+				return u
 			},
 			err: true,
 		},
@@ -53,19 +51,13 @@ func TestUserUsecase_Get(t *testing.T) {
 		tc := cases[i]
 		t.Run(tc.name, func(t *testing.T) {
 			t.Parallel()
-			ctrl := gomock.NewController(t)
-			defer ctrl.Finish()
 
-			userRepo := mock_repository.NewMockUser(ctrl)
-			userRepo.EXPECT().
-				Get(tc.userGet.ArgId).
-				Return(tc.userGet.Ret0, tc.userGet.Ret1).
-				Times(tc.userGet.Time)
+			c := gomock.NewController(t)
+			defer c.Finish()
 
 			lg := applog.New(nil)
-
 			u := Usecase{
-				user: userRepo,
+				user: tc.userRepo(c),
 				log:  lg,
 			}
 			got, aerr := u.Get(lg.TestContext(), tc.give)
@@ -82,12 +74,11 @@ func TestUserUsecase_Get(t *testing.T) {
 
 func TestUserUsecase_Create(t *testing.T) {
 	cases := []struct {
-		name       string
-		give       *CreateInput
-		want       *CreateOutput
-		userCreate mockio_repository.UserCreate
-		userList   mockio_repository.UserList
-		err        bool
+		name     string
+		give     *CreateInput
+		want     *CreateOutput
+		userRepo func(*gomock.Controller) *mock_repository.MockUser
+		err      bool
 	}{
 		{
 			name: "success",
@@ -98,19 +89,11 @@ func TestUserUsecase_Create(t *testing.T) {
 					{Name: "new"},
 				},
 			},
-			userCreate: mockio_repository.UserCreate{
-				Time:    1,
-				ArgUser: &model.User{Name: "new"},
-				Ret0:    nil,
-			},
-			userList: mockio_repository.UserList{
-				Time:   1,
-				ArgIds: nil,
-				Ret0: []*model.User{
-					{Name: "old"},
-					{Name: "new"},
-				},
-				Ret1: nil,
+			userRepo: func(c *gomock.Controller) *mock_repository.MockUser {
+				u := mock_repository.NewMockUser(c)
+				u.EXPECT().Create(&model.User{Name: "new"}).Return(nil)
+				u.EXPECT().List(nil).Return([]*model.User{{Name: "old"}, {Name: "new"}}, nil)
+				return u
 			},
 			err: false,
 		},
@@ -118,10 +101,10 @@ func TestUserUsecase_Create(t *testing.T) {
 			name: "fail to create user",
 			give: &CreateInput{Name: "new"},
 			want: nil,
-			userCreate: mockio_repository.UserCreate{
-				Time:    1,
-				ArgUser: &model.User{Name: "new"},
-				Ret0:    errors.New("unknown"),
+			userRepo: func(c *gomock.Controller) *mock_repository.MockUser {
+				u := mock_repository.NewMockUser(c)
+				u.EXPECT().Create(&model.User{Name: "new"}).Return(errors.New("unknown"))
+				return u
 			},
 			err: true,
 		},
@@ -134,16 +117,11 @@ func TestUserUsecase_Create(t *testing.T) {
 					{Name: "new"},
 				},
 			},
-			userCreate: mockio_repository.UserCreate{
-				Time:    1,
-				ArgUser: &model.User{Name: "new"},
-				Ret0:    nil,
-			},
-			userList: mockio_repository.UserList{
-				Time:   1,
-				ArgIds: nil,
-				Ret0:   nil,
-				Ret1:   errors.New("unknown"),
+			userRepo: func(c *gomock.Controller) *mock_repository.MockUser {
+				u := mock_repository.NewMockUser(c)
+				u.EXPECT().Create(&model.User{Name: "new"}).Return(nil)
+				u.EXPECT().List(nil).Return(nil, errors.New("unknown"))
+				return u
 			},
 			err: true,
 		},
@@ -154,23 +132,12 @@ func TestUserUsecase_Create(t *testing.T) {
 		t.Run(tc.name, func(t *testing.T) {
 			t.Parallel()
 
-			ctrl := gomock.NewController(t)
-			defer ctrl.Finish()
-
-			userRepo := mock_repository.NewMockUser(ctrl)
-			userRepo.EXPECT().
-				Create(tc.userCreate.ArgUser).
-				Return(tc.userCreate.Ret0).
-				Times(tc.userCreate.Time)
-			userRepo.EXPECT().
-				List(tc.userList.ArgIds).
-				Return(tc.userList.Ret0, tc.userList.Ret1).
-				Times(tc.userList.Time)
+			c := gomock.NewController(t)
+			defer c.Finish()
 
 			lg := applog.New(nil)
-
 			u := Usecase{
-				user: userRepo,
+				user: tc.userRepo(c),
 				log:  lg,
 			}
 			got, aerr := u.Create(lg.TestContext(), tc.give)
