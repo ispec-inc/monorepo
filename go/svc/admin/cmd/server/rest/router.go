5bab10f709424e09a17b11671c1316115602e66a
Yusuke Yamada <yuhsukeshootsfc@gmail.com>
2021-06-05 16:29:22 +0900

    [fix] router and subscriber.

--- go/svc/admin/cmd/server/rest/router.go
+++ go/svc/admin/cmd/server/rest/router.go
@@ -9,6 +9,7 @@ import (
 	v1 "github.com/ispec-inc/monorepo/go/svc/admin/pkg/controller/rest/v1"
 	"github.com/ispec-inc/monorepo/go/svc/admin/pkg/database"
 	"github.com/ispec-inc/monorepo/go/svc/admin/pkg/logger"
+	"github.com/ispec-inc/monorepo/go/svc/admin/pkg/redisbs"
 )
 
 func NewRouter() (http.Handler, func() error, error) {
@@ -17,6 +18,10 @@ func NewRouter() (http.Handler, func() error, error) {
 	}
 	cleanup, err := logger.Init()
 	if err != nil {
+		return nil, nil, err
+	}
+
+	if err := redisbs.Init(); err != nil {
 		return nil, nil, err
 	}
 
