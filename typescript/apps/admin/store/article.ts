c282082d76b14e4f75faf271ff33c2a09079f847
Kohta Takanami <ripplekouha@gmail.com>
2021-06-07 17:16:29 +0900

    [fix] protobufのコードを削除

--- typescript/apps/admin/store/article.ts
+++ typescript/apps/admin/store/article.ts
@@ -9,8 +9,6 @@ import {GetRequest, GetResponse, ListResponse} from '@monorepo/gen/admin/api/res
 import { store } from '@/store'
 import { $axios } from '@/utils/api'
 import { Article } from "@monorepo/gen/admin/view/article_pb"
-import { Timestamp } from "google-protobuf/google/protobuf/timestamp_pb"
-import {types} from "node-sass";
 
 @Module({ name: 'article', dynamic: true, store, namespaced: true })
 export class ArticleModule extends VuexModule {
@@ -20,30 +18,7 @@ export class ArticleModule extends VuexModule {
 
   @Mutation
   SET_ARTICLES(value: { articles: Array<Article.AsObject> }): void {
-    const articles = value.articles.map((article: Article.AsObject) => {
-      const articleInstance = new Article()
-      const createdAt: Timestamp | undefined = article.createdAt ? new Timestamp() : undefined
-      const updatedAt: Timestamp | undefined = article.updatedAt ? new Timestamp() : undefined
-      if (createdAt) {
-        createdAt.setSeconds(article.createdAt!.seconds)
-        createdAt.setNanos(article.createdAt!.nanos)
-      }
-      if (updatedAt) {
-        updatedAt.setSeconds(article.updatedAt!.seconds)
-        updatedAt.setNanos(article.updatedAt!.nanos)
-      }
-
-      articleInstance.setId(article.id)
-      articleInstance.setTitle(article.title)
-      articleInstance.setBody(article.body)
-      articleInstance.setCreatedAt(createdAt)
-      articleInstance.setUpdatedAt(updatedAt)
-      return articleInstance
-    })
-    const listResponse = new ListResponse()
-    listResponse.setArticlesList(articles)
-
-    this.listResponse = listResponse
+    // in progress
   }
 
   @Action({rawError: true})
