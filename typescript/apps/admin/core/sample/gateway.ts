f1211ee692f26394118578b06c8c0e6ae6b8ff92
minminzemi <genmu2628@gmail.com>
2021-10-28 14:45:20 +0900

    [update] ディレクトリ構成更新

--- typescript/apps/admin/core/sample/gateway.ts
+++ typescript/apps/admin/core/sample/gateway.ts
@@ -1,11 +1,13 @@
 import { cacheSampleModule } from "~/store/cache/sample"
+import { SampleGetRequest } from "~/types/request/sample/get"
+import { SampleGetResponse } from "~/types/response/sample/get"
 import { ApiWithCacheEnabled } from "~/utils/api-with-cache-enabled"
 
 export class SampleGatewayImpl {
-  private readonly api: ApiWithCacheEnabled<{ title: string, author: string, content: string }, { id: number }>
+  private readonly api: ApiWithCacheEnabled<SampleGetResponse, SampleGetRequest>
 
   constructor() {
-    const endpoint = (payload: { id: number }): Promise<{ title: string, author: string, content: string }> => {
+    const endpoint = (payload: { id: number }): Promise<SampleGetResponse> => {
       console.log('payload', payload)
       return Promise.resolve({
         title: `article ${payload.id}`,
@@ -16,7 +18,7 @@ export class SampleGatewayImpl {
     this.api = new ApiWithCacheEnabled(endpoint, cacheSampleModule)
   }
 
-  fetchById(id: number): Promise<{ title: string, author: string, content: string }> {
+  fetchById(id: number): Promise<SampleGetResponse> {
     return this.api.callApi({ id })
   }
 }
