1fc464da710c65e905c9950b9c3cdedd63d19046
Jason Woodland <me@jasonwoodland.com>
2022-01-14 16:35:31 +1100

    [add] graphqlws handler

--- go/svc/graphql/runner/router/server.go
+++ go/svc/graphql/runner/router/server.go
@@ -6,10 +6,11 @@ import (
 	"github.com/go-chi/chi"
 	graphql "github.com/graph-gophers/graphql-go"
 	"github.com/graph-gophers/graphql-go/relay"
+	"github.com/graph-gophers/graphql-transport-ws/graphqlws"
+	mw "github.com/ispec-inc/monorepo/go/pkg/middleware"
+	"github.com/ispec-inc/monorepo/go/svc/graphql/pkg/config"
 	"github.com/ispec-inc/monorepo/go/svc/graphql/pkg/database"
 	"github.com/ispec-inc/monorepo/go/svc/graphql/pkg/handler"
-	"github.com/ispec-inc/monorepo/go/svc/graphql/pkg/config"
-	mw "github.com/ispec-inc/monorepo/go/pkg/middleware"
 	"github.com/ispec-inc/monorepo/go/svc/graphql/pkg/middleware"
 	"github.com/ispec-inc/monorepo/go/svc/graphql/pkg/schema"
 )
@@ -26,7 +27,7 @@ func NewGraphQL() (http.Handler, func() error, error) {
 
 	schema := graphql.MustParseSchema(s, handler.New())
 
-	h := &relay.Handler{Schema: schema}
+	h := graphqlws.NewHandlerFunc(schema, &relay.Handler{Schema: schema})
 	r := chi.NewRouter()
 	r = mw.Common(r, mw.CommonConfig{
 		Timeout:      config.Router.Timeout,
