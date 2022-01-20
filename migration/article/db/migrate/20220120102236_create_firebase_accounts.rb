8e9424b65cfcc8be334e6fd40abf3b20d96d72b1
mokoshin0720 <shinya.work0720@gmail.com>
2022-01-20 19:25:03 +0900

    [add]migration

---
+++ migration/article/db/migrate/20220120102236_create_firebase_accounts.rb
@@ -0,0 +1,19 @@
+class CreateFirebaseAccounts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :firebase_accounts do |t|
+      t.timestamps
+    end
+
+    create_table :firebase_account_details do |t|
+      t.references :firebase_account
+      t.string :firebase_service_id, null:false, default: ""
+      t.timestamps
+    end
+
+    create_table :firebase_account_users do |t|
+      t.references :firebase_accounts
+      t.references :user
+      t.timestamps
+    end
+  end
+end
