e4024550071354992022e71baab2663eb984d9cf
Kohta Takanami <ripplekouha@gmail.com>
2022-03-30 20:19:56 +0900

    [fix] unocss の typeファイルを修正

--- typescript/apps/next-apollo/@types/shims.d.ts
+++ typescript/apps/next-apollo/@types/shims.d.ts
@@ -1,15 +1,4 @@
 /* eslint-disable */
-// import { AriaAttributes, DOMAttributes } from 'react';
-
-// declare module 'react' {
-//   interface HTMLAttributes<T> extends AriaAttributes, DOMAttributes<T> {
-//     w?: string
-//     h?: string
-//     flex?: string | boolean
-//     items?: string | boolean
-//     justify?: string | boolean
-//   }
-// }
 import { AttributifyAttributes, AttributifyNames } from 'windicss/types/jsx'
 type AttributesType = string
 type CustomAttributifyAttributes = Partial<Record<AttributifyNames, AttributesType>>
